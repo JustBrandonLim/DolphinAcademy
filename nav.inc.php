7148c0c72b08cfd6512cc399413e13ca1acc1956
brand <brand@BRANDON-LAPTOP>
2021-11-04 14:52:43 +0800

    Added Zephyr Bootstrap Theme

--- nav.inc.php
+++ nav.inc.php
@@ -11,7 +11,7 @@
         <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="#">Home
+                    <a class="nav-link active" href="./index.php">Home
                         <span class="visually-hidden">(current)</span>
                     </a>
                 </li>
---
+++ products.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            include "includes.inc.php";
+        ?>
+    </head>
+    <body>
+        <?php
+            include "nav.inc.php";
+        ?>
+        <header class="jumbotron text-center">
+            <h1 class="display-4">Products</h1>
+            <h2>Home of Learners</h2>
+        </header>
+    </body>
+    <footer>
+        
+    </footer>
+</html>
