77789e7e2488bd208cb28c0f9c9330a327250646
Brandon Lim <brandonlimjingzhi@gmail.com>
2021-11-12 16:48:30 +0800

    Updated products.php

--- products.php
+++ products.php
@@ -10,60 +10,63 @@
             include "nav.inc.php";
         ?>
         <main class="container">  
-            
-            <?php
-                $errorMsg = "";
-                $success = true;
-                
-                getProducts();
-                
-                if (!$success)
-                {
-                    echo $errorMsg;
-                }
-            
-                function getProducts()
-                {
-                    global $errorMsg;
-                    
-                    // Create database connection.
-                    $config = parse_ini_file('./db-config.ini');
-                    //$config = parse_ini_file('../../private/db-config.ini');
-                    $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
-                    
-                    // Check connection
-                    if ($conn->connect_error)
+            <div class="row">
+                <?php
+                    $errorMsg = "";
+                    $success = true;
+
+                    getProducts();
+
+                    if (!$success)
                     {
-                        $errorMsg = "Connection failed! Error: " . $conn->connect_error;
-                        $success = false;
+                        echo $errorMsg;
                     }
-                    else
+
+                    function getProducts()
                     {
-                        // Prepare the statement:
-                        $stmt = $conn->prepare("SELECT * FROM dolphin_academy_products");
-                        $stmt->execute();
-                        $result = $stmt->get_result();
-                        if ($result->num_rows > 0)
+                        global $errorMsg;
+
+                        // Create database connection.
+                        $config = parse_ini_file('./db-config.ini');
+                        //$config = parse_ini_file('../../private/db-config.ini');
+                        $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+
+                        // Check connection
+                        if ($conn->connect_error)
                         {
-                            while($row = $result->fetch_assoc()) {
-                                echo "<div class=\"card\" style=\"width: 18rem;\">";
-                                echo "<div class=\"card-body\">";
-                                echo "<h5 class=\"card-title\">" . $row["name"] . "/h5>";
-                                echo "<p class=\"card-text\">" . $row["description"] . "</p>";
-                                echo "</div>";
-                                echo "</div>";
-                            }
+                            $errorMsg = "Connection failed! Error: " . $conn->connect_error;
+                            $success = false;
                         }
                         else
                         {
-                            $errorMsg = "Products not available!";
-                            $success = false;
+                            // Prepare the statement:
+                            $stmt = $conn->prepare("SELECT * FROM dolphin_academy_products");
+                            $stmt->execute();
+                            $result = $stmt->get_result();
+                            if ($result->num_rows > 0)
+                            {
+                                while($row = $result->fetch_assoc()) {
+                                    echo "<div class=\"col\">";
+                                    echo "<div class=\"card\" style=\"width: 18rem;\">";
+                                    echo "<div class=\"card-body\">";
+                                    echo "<h5 class=\"card-title\">" . $row["name"] . "</h5>";
+                                    echo "<p class=\"card-text\">" . $row["description"] . "</p>";
+                                    echo "</div>";
+                                    echo "</div>";
+                                    echo "</div>";
+                                }
+                            }
+                            else
+                            {
+                                $errorMsg = "Products not available!";
+                                $success = false;
+                            }
+                            $stmt->close();
                         }
-                        $stmt->close();
+                        $conn->close();
                     }
-                    $conn->close();
-                }
-            ?>
+                ?>
+            </div>
         </main>
     </body>
     <?php
