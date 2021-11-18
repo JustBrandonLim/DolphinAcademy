60bddb871408b04eae8502af7c4808138ac2334d
Brandon Lim <brandonlimjingzhi@gmail.com>
2021-11-18 18:32:08 +0800

    Merge origin/master
    Conflicts:
    	testimonial.php

--- admin.php
+++ admin.php
@@ -61,11 +61,43 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-auto">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Update Product</h5>
+                            <p class="card-text">This function allows you to update products.</p>
+                            <form action="process_register.php" method="post">  
+                                <div class="form-group">
+                                    <label for="pname">Product Name:</label>
+                                    <select class="form-select" id="pname" name="pname" aria-label="Products">
+                                        <option selected>Test Product 1</option>
+                                        <option value="1">Test Product 1</option>
+                                        <option value="2">Test Product 1</option>
+                                        <option value="3">Test Product 1</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="pname">Product Name:</label>
+                                    <input class="form-control" type="text" id="pname" maxlength="45" name="pname" 
+                                            placeholder="Enter product name">
+                                </div>
+
+                                <div class="form-group">
+                                        <label for="pdesc">Product Description:</label>
+                                        <textarea class="form-control" id="pdesc" maxlength="255" name="pdesc" 
+                                                placeholder="Enter product description"></textarea>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <button class="btn btn-primary" type="submit">Delete Product</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
                 <?php
                     require "php/DatabaseFunctions.php";
                 ?>
-                
-                <p></p>
             </div>
         </main>
     </body>
