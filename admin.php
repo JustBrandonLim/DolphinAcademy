da05b560051f20f0d68020725d908d380fbc86e1
Brandon Lim <brandonlimjingzhi@gmail.com>
2021-11-18 14:52:22 +0800

    Update

---
+++ admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            include "includes.inc.php";
+        ?>
+    </head>
+    <body>
+        <?php
+            include "nav.inc.php";
+        ?>
+        <main class="container-fluid">  
+            <div class="row">
+                <?php
+                    require "php/DatabaseFunctions.php";
+                    
+             
+                ?>
+            </div>
+        </main>
+    </body>
+    <?php
+        include "footer.inc.php"; 
+    ?>
+</html>
