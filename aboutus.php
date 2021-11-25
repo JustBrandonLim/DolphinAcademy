71586eaf5efe5d34c9a7552deaa97dbcb6be6665
elysi <elysi@LAPTOP-H1NU4D8M>
2021-11-25 21:17:59 +0800

    carousel on about us

--- aboutus.php
+++ aboutus.php
@@ -6,16 +6,67 @@
             include "./includes.inc.php";
         ?>
     </head>
-    <body>
+    <body style="background-color: #123;color: white;">
         <?php
             include "./nav.inc.php";
         ?>
         <header>
-            <h1>About Us</h1>
-            <p>Home of the Learners</p>
+            <h1 style="color:white;">About Us</h1>
+            <p>Introducing the 4 developers of Dolphin Academy</p>
         </header>
-        <main class="container-fluid">
-            
+        <main>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="2000">
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+                <div class="carousel-inner container">
+                    <div class="carousel-item active">
+                        <div class="container">
+                            <img class="d-block w-100" src="assets/cool.jpg" alt="First slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h2 style="color: white;">Brandon</h2>
+                              <p>description</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="container">
+                            <img class="d-block w-100" src="assets/cool.jpg" alt="Second slide">
+                            <div class="carousel-caption d-none d-md-block">
+                              <h2 style="color: white;">Priya</h2>
+                              <p>description</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                      <div class="container">
+                          <img class="d-block w-100" src="assets/cool.jpg" alt="Third slide">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h2 style="color: white;">Ismael</h2>
+                            <p>description</p>
+                          </div>
+                      </div>
+                    </div>
+                    <div class="carousel-item">
+                      <div class="container">
+                          <img class="d-block w-100" src="assets/cool.jpg" alt="Fourth slide">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h2 style="color: white;">Elysia</h2>
+                            <p>description</p>
+                          </div>
+                      </div>
+                    </div>
+                </div>
+                  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  </a>
+                  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  </a>
+            </div>
         </main>
         <?php
             include "./footer.inc.php"; 
