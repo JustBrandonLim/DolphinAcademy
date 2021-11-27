a5368bcca1945ee669e10bfe93f3af560dda29b4
Brandon <brandonlimjingzhi@gmail.com>
2021-11-27 23:10:54 +0800

    bugfixxxxxx FUCK MY LIFE index

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
             include "./nav.inc.php";
         ?>
         <main>
-            <div id ="main" class="container-fluid p-0" class="jumbotron jumbotron-fluid">
+            <div id ="main" class="container-fluid p-0">
                 <section class="first-quote-jumbotron">
                     <a href="courses.php"><span>"Learn continually - there's always<br>'one more thing' to learn," - Steve Jobs </span></a>
                 </section>
-                <section class="second-quote-jumbotron">
+                <section class="jumbotron">
                     <a href="courses.php"><span>Maximize learning opportunities with us!</span></a>
                 </section>
             </div>
