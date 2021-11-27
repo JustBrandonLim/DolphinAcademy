2f6c4911f4e0e3cd2ffbc7b9fa2b141b2946dfcf
Brandon <brandonlimjingzhi@gmail.com>
2021-11-27 22:20:04 +0800

    fix aboutus

--- aboutus.php
+++ aboutus.php
@@ -10,21 +10,29 @@
         <?php
             include "./nav.inc.php";
         ?>
-        <main>
-            <section class="three">
-                <img class="image-fluid"src="../images/member2.jpg" width="250" height="400" alt="alt"/>
-                <h2><strong>GEORGE TANNER</strong><br>Founder of Dolphin Academy</h2>
-            </section>
-            <section class="four">
-                <p>Founded in 2021, Dolphin Academy is passionate and dedicated in providing courses to those willing to learn! 
-                    With a wide variety of courses, we are sure there's something for everyone!
-                    We provide top quality materials no matter the course!
-                    Due to the ever-changing conditions caused by global events, learning from home has been the norm.
-                    We seek to provide a venue with quality courses for those willing to take up new challenges!</p>
-            </section>
-                    
-
-
+        <main class="container-fluid">
+            <header>
+                <h1>About Us</h1>
+            </header>
+            <div class="row justify-content-center">
+                <div class="col-auto">
+                    <div class="card large-card">
+                        <h5 class="card-header">
+                            Our Founder
+                        </h5>
+                        <img src="./images/founder.jpg" class="card-img-top" width="250" height="400" alt="founder_image">
+                        <div class="card-body">
+                            <h5 class="card-title">George Tanner</h5>
+                            <p class="card-text">
+                                Founded in 2021, Dolphin Academy is passionate and dedicated in providing courses to those willing to learn! 
+                                With a wide variety of courses, we are sure there's something for everyone!
+                                We provide top quality materials no matter the course!
+                                Due to the ever-changing conditions caused by global events, learning from home has been the norm.
+                                We seek to provide a venue with quality courses for those willing to take up new challenges!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
         <?php
             include "./footer.inc.php"; 
